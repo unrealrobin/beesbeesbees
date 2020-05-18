d236f93f674415386d71e12f21657a45fdb4f91d
robinnnnnn <rlifshitz19@gmail.com>
2020-05-18 11:47:29 -0700

    BeesBeesBees Inherit Using Pseudoclassical Classes All Tests Passing

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-var Bee = function() {
+var Bee = function(){
+  Grub.call(this); //calls the superclass, you need this everytime
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = "keep on growing"
+
 };
 
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
